3a7a7a4cf0381835e90774b113c4273d33ee54f1
GABRIELA BREITENBERGER <g_breitenberger@hotmail.com>
2022-02-20 19:32:38 -0300

    actualizacion

--- src/Component/CartWidget/SliderCart.js
+++ src/Component/CartWidget/SliderCart.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './SliderCart.css';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -13,6 +14,8 @@ export default function SliderCart() {
 
   const {cart, total} = useCartContext()
 
+  const [product, setProduct] = useState()
+
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
@@ -20,25 +23,32 @@ export default function SliderCart() {
 
     setState({ right: open });
   };
+  const tachoDeBasura = () => {
+
+    setProduct([]);
+  }
 
   const list = (anchor) => (
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 350 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}    
     >
+   
       <List>
         {cart.length == 0 ? <h2>No hay productos<br></br> en el carrito</h2> : 
         cart.map((x) => 
           <>
+          <img className='imagenCarrito' src={x.imagen} alt="logo"></img>
           <p>Nombre: {x.title}</p>
           <p>Precio: ${x.precio}</p>
           <p>Cantidad: {x.qty}</p>
-          </>)
+          <button onClick={tachoDeBasura}><img src={tachoDeBasura} alt="logo"></img></button>
+</>)
           }
         
-        <h4>Total: ${total()}</h4>
+        <h4>Total: ${total()}</h4>        
       </List>
     </Box>
   );
@@ -47,7 +57,8 @@ export default function SliderCart() {
     <div>
       {['right'].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}><img className='logoCarrito' src={IconoCart} alt="logo"/></Button>
+          <Button onClick={toggleDrawer(anchor, true)}><img className='logoCarrito' src={IconoCart} alt="logo"/><span className='contadorCarrito'>0</span>0</Button>
+          
           <Drawer
             anchor={anchor}
             open={state[anchor]}
@@ -56,6 +67,7 @@ export default function SliderCart() {
             {list(anchor)}
           </Drawer>
         </React.Fragment>
+       
       ))}
     </div>
   );
